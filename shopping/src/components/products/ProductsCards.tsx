670a2e14b57d7cb970380008cc130cb31b6cf060
Matheus Patricio <contato.matheuspatricio@gmail.com>
2024-07-15 18:20:57 -0300

    pushing api request to browser

--- shopping/src/components/products/ProductsCards.tsx
+++ shopping/src/components/products/ProductsCards.tsx
@@ -1,34 +1,46 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios, { AxiosInstance } from "axios";
+import { EffectCallback, useEffect, useState } from "react";
 import * as S from "./cardsStyles"
 import { FiShoppingCart } from "react-icons/fi";
 
-
+type ObjectData = {
+    id: number,
+    image: string,
+    title: string,
+    price: number
+}
 
 export const ProductsCards: React.FC = () => {
-const [data, setData] = useState([])
+const [data, setData] = useState<ObjectData[]>([])
 
+    useEffect(() => {
+        axios.get('https://fakestoreapi.com/products')
+        .then((response) => {
+            setData(response.data)
+            console.log(data)
+        }).catch((err) => {
+            console.log("erro:", err)
+        })
+    }, [])
 
-useEffect(() => {
-  fetch('https://fakestoreapi.com/products')
-   .then(response => response.json())
-   .then(data => {
-    setData(data)
-    console.log(data)
-   })
-}, [])
-
-
-
-
+    
     return(
         <S.Card>
+            {data.map(card => {
+                return(
+                    <li key={card.id}>
+                        <img src={card.image}/>
+                        <h2>{card.title}</h2>
+                        <h4>{card.price}</h4>
+                    </li>
+                )
+            })}
             <S.ProductImage/>
             <S.ProductTitle></S.ProductTitle>
 
             <S.ReviewContainer>
                 <S.Review>4</S.Review>
-                <S.Price>R$10</S.Price>
+                <S.Price></S.Price>
 
             </S.ReviewContainer>
 
